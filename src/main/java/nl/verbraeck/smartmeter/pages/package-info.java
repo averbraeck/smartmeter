2c6c47c167057e40f6f16c0fec777307012ed8da
Alexander Verbraeck <a.verbraeck@gmail.com>
2023-05-18 21:14:09 +0200

    Do not create a separate pages package.

--- src/main/java/nl/verbraeck/smartmeter/pages/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * Generating the page content for the different pages of the application.
- * <p>
- * Copyright (c) 2020-2023 Alexnder Verbraeck, Delft, the Netherlands. All rights reserved. <br>
- * MIT-license.
- * </p>
- * @author <a href="https://github.com/averbraeck">Alexander Verbraeck</a>
- */
-package nl.verbraeck.smartmeter.pages;
